877df0d581f72983a8cf06a697f9f72ae69f677c
lorenzo-77 <lorenzo.geraldo.munoz@gmail.com>
2022-03-21 12:14:06 -0300

    Se agrega El Proyecto

---
+++ capaDatos/CDEmpleados.vb
@@ -0,0 +1,74 @@
+﻿Imports MySql.Data.MySqlClient
+Imports capaEntidad
+Public Class CDEmpleados
+
+    Private _cadenaConexion As String = "Server=127.0.0.1;User=root;Password=123;Port=3306;database=crud_empleado"
+
+    Public Sub ProbarConexion()
+
+        Dim Conexion As New MySqlConnection(_cadenaConexion)
+
+        Try
+            Conexion.Open()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            Exit Sub
+        End Try
+
+        MessageBox.Show("Conexion con Exito")
+        Conexion.Close()
+
+    End Sub
+
+    Public Sub Insertar(ByVal empleado As CEEmpleado)
+
+        Dim Conexion As New MySqlConnection(_cadenaConexion)
+        Conexion.Open()
+        Dim Query As String = "INSERT INTO `empleados` (`nombre`,`apellido`,`foto`) 
+                               VALUES ('" & empleado.Nombre & "', '" & empleado.Apellido & "', 
+                               '" & MySql.Data.MySqlClient.MySqlHelper.EscapeString(empleado.Foto) & "');"
+        Dim Comando As New MySqlCommand(Query, Conexion)
+        Comando.ExecuteNonQuery()
+        Conexion.Close()
+        MessageBox.Show("Registro Creado")
+
+    End Sub
+
+    Public Sub Modificar(ByVal empleado As CEEmpleado)
+        Dim Conexion As New MySqlConnection(_cadenaConexion)
+        Conexion.Open()
+        Dim Query As String = "UPDATE `empleados` SET `nombre`='" & empleado.Nombre & "', `apellido`='" & empleado.Apellido & "',
+                               `foto`='" & MySql.Data.MySqlClient.MySqlHelper.EscapeString(empleado.Foto) & "' 
+                               WHERE  `id`=" & empleado.Id & ";"
+
+        Dim Comando As New MySqlCommand(Query, Conexion)
+        Comando.ExecuteNonQuery()
+        Conexion.Close()
+        MessageBox.Show("Registro Editado")
+    End Sub
+    Public Sub Eliminar(ByVal empleado As CEEmpleado)
+        Dim Conexion As New MySqlConnection(_cadenaConexion)
+        Conexion.Open()
+        Dim Query As String = "DELETE FROM `empleados` WHERE  `id`=" & empleado.Id & ";"
+        Dim Comando As New MySqlCommand(Query, Conexion)
+        Comando.ExecuteNonQuery()
+        Conexion.Close()
+        MessageBox.Show("Registro Eliminado")
+    End Sub
+
+    Public Function Listar() As DataSet
+
+        Dim Conexion As New MySqlConnection(_cadenaConexion)
+        Conexion.Open()
+        Dim Query As String = "SELECT * FROM `empleados` LIMIT 1000;"
+        Dim Adapatador As MySqlDataAdapter
+        Dim dataSet As New DataSet
+
+        Adapatador = New MySqlDataAdapter(Query, Conexion)
+        Adapatador.Fill(dataSet, "Empleado")
+
+        Return dataSet
+
+    End Function
+
+End Class
---
+++ capaNegocio/CNEmpleado.vb
@@ -0,0 +1,46 @@
+﻿Imports capaEntidad
+Imports capaDatos
+Public Class CNEmpleado
+
+    Dim DatosEmpleados As New CDEmpleados
+    Public Function ValidarDatos(ByVal empleado As CEEmpleado) As Boolean
+        Dim Resultado As Boolean = True
+
+        If empleado.Nombre = "" Then
+            Resultado = False
+            MessageBox.Show("Completar el campo de Nombre")
+        End If
+        If empleado.Apellido = "" Then
+            Resultado = False
+            MessageBox.Show("Completar el campo de Apellido")
+        End If
+
+        Return Resultado
+    End Function
+    Public Sub PruebaMySql()
+
+        DatosEmpleados.ProbarConexion()
+
+    End Sub
+
+    Public Sub Insertar(ByRef empleado As CEEmpleado)
+        DatosEmpleados.Insertar(empleado)
+    End Sub
+
+    Public Sub Editar(ByVal empleado As CEEmpleado)
+        DatosEmpleados.Modificar(empleado)
+    End Sub
+
+    Public Sub Eliminar(ByVal empleado As CEEmpleado)
+
+        If MessageBox.Show("¿Estas Seguro?", "Pregunta", MessageBoxButtons.YesNo, MessageBoxIcon.Question) = DialogResult.Yes Then
+            DatosEmpleados.Eliminar(empleado)
+        End If
+
+    End Sub
+
+    Public Function Listar() As DataSet
+        Return DatosEmpleados.Listar()
+    End Function
+
+End Class
